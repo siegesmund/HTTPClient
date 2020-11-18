4473685218a4540b4cac7a5cec90a04b43b85cd3
Peter Siegesmund <peter.siegesmund@icloud.com>
2020-11-18 12:26:51 -0500

    broken, but with more refactoring

--- Sources/HTTPClient/HTTPBaseClient.swift
+++ Sources/HTTPClient/HTTPBaseClient.swift
@@ -2,29 +2,113 @@ import Foundation
 import Combine
 import Alamofire
 
-public protocol HTTPBaseClient: URLBuildable {}
+
+enum HTTPClientRequestError: Error {
+    case decodingError
+    case other(Error)
+}
+
+struct HTTPClientRequest<T:Codable,U:Alamofire.RequestInterceptor> {
+    
+    var url: URL
+    var interceptor: U?
+    var error: Error?
+    let timestamp: Date = Date()
+    
+    internal var dataRequest: DataRequest {
+        return self.interceptor == nil ? AF.request(url) : AF.request(url, interceptor: self.interceptor)
+    }
+    
+    var result: AnyPublisher<T,Error> {
+        return dataRequest
+            .publishData()
+            .result()
+            .map { try! $0.get() }
+            .decode(type: T.self, decoder: JSONDecoder())
+            /*.mapError { (error) -> HTTPClientRequestError in
+                switch error {
+                case is Swift.DecodingError:
+                    return .decodingError
+                default:
+                    return .other(error)
+                }
+            }*/
+            .eraseToAnyPublisher()
+    }
+    
+    var publisher: AnyPublisher<HTTPClientRequest,Never> {
+        return Just(self).eraseToAnyPublisher()
+    }
+    
+    init(url: URL, interceptor: U? = nil) {
+        self.url = url
+        self.interceptor = interceptor
+    }
+}
+
+public protocol HTTPBaseClient: URLBuildable {
+    static var log: Bool { get }
+    static func prefetch()
+    static func postfetch()
+}
 
 extension HTTPBaseClient {
     
-    internal static func log(data: Data) -> Data {
-        print(String(decoding: data, as: UTF8.self))
+    var log: Bool { return true }
+    
+    static func prefetch<T:Codable, U: Alamofire.RequestInterceptor>(_ request: HTTPClientRequest<T,U>) -> AnyPublisher<HTTPClientRequest<T,U>,Never> {
+        print("Running prefetch \(request.url.absoluteString)")
+        return Just(request).eraseToAnyPublisher()
+    }
+    
+    static func postfetch() {
+        print("Running postfetch")
+    }
+    
+    internal static func _log(data: Data) -> Data {
+        // if log is true, log the data
+        log ? print(String(decoding: data, as: UTF8.self)) : nil
+        
         return data
     }
-    
-    internal static func _dataRequest<T:Alamofire.RequestInterceptor>(url: URL, interceptor: T? = nil) -> DataRequest {
-        return interceptor == nil ? AF.request(url) : AF.request(url, interceptor: interceptor)
+    /*
+    internal static func _dataRequest<T:Codable, U:Alamofire.RequestInterceptor>(_ request: HTTPClientRequest<T,U>) -> AnyPublisher<HTTPClientRequest<T,U>,Error> {
+        
+        var afRequest: DataRequest = (request.interceptor == nil ?
+                                        AF.request(request.url) :
+                                        AF.request(request.url, interceptor: request.interceptor))
+            .publishData()
+            .result()
+            .map { HTTPClientRequest(url: request.url, interceptor: request.interceptor, result: $0, error: nil) }
+        
+        return afRequest
+            .publishData()
+            .result()
+            .setFailureType(to: Error.self)
+
+            .map(getData)
+            .decode(type: T.self, decoder: JSONDecoder())
+            .map {}
+            .eraseToAnyPublisher()
     }
     
-    internal static func _processDataRequest<T:Codable>(dataRequest: DataRequest) -> AnyPublisher<T,Error> {
+    
+    internal static func getData(_ result: Result<Data,AFError>) -> Data {
+        return try! result.get()
+    }
+    */
+    /*
+    internal static func _processDataRequest<T:Codable>(_ dataRequest: DataRequest) -> AnyPublisher<T,Error> {
         return dataRequest
             .publishData()
             .result()
             .setFailureType(to: Error.self)
-            .map { try! $0.get() }
-            .map(log)
+            .map(getData)
+            .map(_log)
             .decode(type: T.self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
+    */
 }
 
 // Requests that utilize interceptors
@@ -35,8 +119,10 @@ extension HTTPBaseClient {
     ///   - url: A URL object
     ///   - interceptor: An instance of JWTRequestInterceptor
     /// - Returns: A publisher that returns an object that conforms to Codable, or an error
-    internal static func _request<T:Codable, U: Alamofire.RequestInterceptor>(url: URL, interceptor: U?) -> AnyPublisher<T,Error> {
-        let request = _dataRequest(url: url, interceptor: interceptor)
-        return _processDataRequest(dataRequest: request)
+    internal static func _request<T:Codable, U: Alamofire.RequestInterceptor>(url: URL, interceptor: U?) -> AnyPublisher<T, HTTPClientRequestError> {
+        return HTTPClientRequest<T,U>(url: url, interceptor: interceptor)
+            .publisher.setFailureType(to: Error.self)
+            .flatMap({ r in r.request })
+            // .flatMap { (r: HTTPClientRequest<T,U>) -> T in return r.result }
     }
 }
